931a9c8a099bcb85ed37a448ed506c2175f11325
atari-monk <atari.monk1@gmail.com>
2024-02-23 20:47:11 +0100

    collectibles

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    int coins = 0;
+
+    [SerializeField] Text coinsText;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Coin")) 
+        {
+            Destroy(other.gameObject);
+            coins++;
+            coinsText.text = "Coins: " + coins;
+        }
+    }
+}
--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -3,17 +3,26 @@ using UnityEngine.SceneManagement;
 
 public class PlayerLife : MonoBehaviour
 {
+    bool dead = false;
+
+    private void Update() {
+        if (transform.position.y < -1f && !dead) {
+            Die();
+        }
+    }
+
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.CompareTag("Enemy Body")) {
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<PlayerMovement>().enabled = false;
             Die();
         }
     }
 
     private void Die() {
-        GetComponent<MeshRenderer>().enabled = false;
-        GetComponent<Rigidbody>().isKinematic = true;
-        GetComponent<PlayerMovement>().enabled = false;
         Invoke(nameof(ReloadLevel), 1.3f);
+        dead = true;
     }
 
     void ReloadLevel() {
