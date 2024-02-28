3a6e24d2a36a8d5c70828e6e302b8542d5a471cd
atari-monk <atari.monk1@gmail.com>
2024-02-28 21:22:40 +0100

    level 2

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.name == "Player") 
+        {
+            
+        }
+    }
+}
