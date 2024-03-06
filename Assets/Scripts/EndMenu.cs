35da7a87348d0cd8df780a17243c94547fca4fc7
atari-monk <atari.monk1@gmail.com>
2024-03-06 21:29:21 +0100

    finish game

---
+++ Assets/Scripts/EndMenu.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class EndMenu : MonoBehaviour
+{
+    public void EndGame() {
+        Application.Quit();
+    }
+}
--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.name == "Player") 
         {
-            
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
